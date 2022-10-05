d074ce23797dc8f1e151c4f252b8056466397b81
SturmEnte <sturmente@proton.me>
2022-10-05 17:28:48 +0200

    Added function that removes the last character of a string

--- src/utility.rs
+++ src/utility.rs
@@ -1,5 +1,10 @@
 mod binary;
+mod string;
 
 pub fn decimal_to_binary(decimal: u8) -> String {
     binary::decimal_to_binary(decimal)
+}
+
+pub fn remove_last_char(string: &str) -> &str {
+    string::remove_last_char(string)
 }
