f21f64237b1d5e1ce5fc6c2d1218db33ad7aee8f
SturmEnte <sturmente@proton.me>
2022-10-05 12:21:07 +0200

    Added if statement that ensures that you enter at least a command

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Please enter a command. You can see all commands witht the help command (stegolib help)");
+        return;
+    }
     
     match args[1].as_str() {
         "lsb" => commands::least_significant_bit(args),
