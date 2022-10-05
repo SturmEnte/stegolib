4aa7ef82cf63e2447af550ab98feed21f5f7d959
SturmEnte <sturmente@proton.me>
2022-10-05 12:56:22 +0200

    Added mode argument

--- src/commands/least_significant_bit.rs
+++ src/commands/least_significant_bit.rs
@@ -1,14 +1,36 @@
 
-// [Cmd name] [input-image] [input-file] [output-image]
+// [Cmd name] [en- or decode] [input-image] [input-file] [output-image]
 pub fn least_significant_bit(args: Vec<String>) {
 
-    if args.len() < 5 {
+    if args.len() < 6 {
         println!("Wrong command usage!");
-        println!("Please use this command like this: {} [input-image] [input-file] [output-image]", args[1]);
+        println!("Please use this command like this: {} [mode] [input-image] [input-file] [output-image]", args[1]);
         return;
     }
 
-    args.iter().for_each(|arg| {
-        println!("{}", arg);
-    });
+    let mode: &String = &args[2];
+    let input_img: &String = &args[3];
+    let input_file: &String = &args[4];
+    let output_img: &String = &args[5];
+
+    println!("Mode: {mode}");
+    println!("Input image: {input_img}");
+    println!("Input file: {input_file}");
+    println!("Output image: {output_img}");
+
+    if mode == "encode" {
+        encode();
+        return;
+    } else if mode == "decode" {
+        println!("Not implemented yet");
+        return;
+    }
+
+    println!("Not an valid mode!");
+    println!("Valid modes: encode, decode");
+    
+}
+
+fn encode() {
+    }
 }
