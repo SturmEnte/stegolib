47ee79c1d0b054f7acfc247a07c498bd146733a9
SturmEnte <sturmente@proton.me>
2022-10-05 18:19:11 +0200

    Added a function that converts binary into decimal

--- src/utility.rs
+++ src/utility.rs
@@ -5,6 +5,10 @@ pub fn decimal_to_binary(decimal: u8) -> String {
     binary::decimal_to_binary(decimal)
 }
 
+pub fn binary_to_decimal(binary: String) -> u8 {
+    binary::binary_to_decimal(binary)
+}
+
 pub fn remove_last_char(string: &str) -> &str {
     string::remove_last_char(string)
 }
--- src/utility/binary.rs
+++ src/utility/binary.rs
@@ -1,5 +1,4 @@
 pub fn decimal_to_binary(decimal: u8) -> String {
-
     let mut result: String = String::new();
     let mut rest: i16 = decimal as i16;
 
@@ -17,4 +16,23 @@ pub fn decimal_to_binary(decimal: u8) -> String {
     }
 
     result
+}
+
+pub fn binary_to_decimal(binary: String) -> u8 {
+    let mut result: u8 = 0;
+
+    let binary_vec: Vec<char> = binary.chars().collect();
+
+    let mut n: i8 = 7;
+    binary_vec.iter().for_each(|char| {
+        result += char_to_u8(char) * 2u8.pow(n as u32);
+        n -= 1;
+    });
+
+    result
+}
+
+fn char_to_u8(char: &char) -> u8 {
+    const RADIX: u32 = 10;
+    char.to_digit(RADIX).unwrap().try_into().unwrap()
 }
