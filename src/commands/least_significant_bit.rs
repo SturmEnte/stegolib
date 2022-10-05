2f239222a97fbd5675cbf9874d771d3d6767d871
SturmEnte <sturmente@proton.me>
2022-10-05 12:25:32 +0200

    Added a check to the lsb command for the right amount of arguments

--- src/commands/least_significant_bit.rs
+++ src/commands/least_significant_bit.rs
@@ -1,5 +1,13 @@
 
+// [Cmd name] [input-image] [input-file] [output-image]
 pub fn least_significant_bit(args: Vec<String>) {
+
+    if args.len() < 5 {
+        println!("Wrong command usage!");
+        println!("Please use this command like this: {} [input-image] [input-file] [output-image]", args[1]);
+        return;
+    }
+
     args.iter().for_each(|arg| {
         println!("{}", arg);
     });
