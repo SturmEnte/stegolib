e667b92c53d8d7e9d2cd070c5f1f34330a5aad4c
SturmEnte <sturmente@proton.me>
2022-10-11 00:56:46 +0200

    Fixed copy error

--- src/error_messages.rs
+++ src/error_messages.rs
@@ -1,5 +1,5 @@
 pub fn wrong_command_usage(command_name: &String) {
     println!("Wrong command usage!");
     println!("Please use this command like this: {} encode [input-image] [input-file] [output-image]", command_name);
-    println!("Or like this: {} decode [input-image] [input-file] [output-image]", command_name);
+    println!("Or like this: {} decode [input-image] [output-file]", command_name);
 }
