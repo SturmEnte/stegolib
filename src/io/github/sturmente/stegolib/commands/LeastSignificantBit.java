f2dc10e84383b19684b3dc8c92b6748c1b35c55c
Jonas <sturmente@proton.me>
2023-02-16 15:17:49 +0100

    Added encode and decode modes

--- src/io/github/sturmente/stegolib/commands/LeastSignificantBit.java
+++ src/io/github/sturmente/stegolib/commands/LeastSignificantBit.java
@@ -11,11 +11,39 @@ public class LeastSignificantBit {
 	public void run() {
 		System.out.println("Least Significant Bit");
 		
-		if(args.length < 4) {
+		if(args.length < 2) {
 			System.out.println("Not enough arguments. Use the help command for further information.");
 			return;
 		}
+		
+		switch(args[1]) {
+			case "encode":
+				if(args.length < 5) {
+					System.out.println("Not enough arguments. Use the help command for further information.");
+					break;
+				}
+				encode();
+				break;
+			case "decode":
+				if(args.length < 4) {
+					System.out.println("Not enough arguments. Use the help command for further information.");
+					break;
+				}
+				decode();
+				break;
+			default:
+				System.out.println("Mode \"" + args[1] + "\" not found! Valid modes: encode, decode. Use the help command for further information.");
+				break;
+		}
 			
+	}
+	
+	private void encode() {
+		System.out.println("Mode: Encode");
+	}
+	
+	private void decode() {
+		System.out.println("Mode: Decode");
 	}
 	
 	public static void help() {
