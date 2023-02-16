a959a297d5dd6b85ae6e63bc07dcda5db51950b5
Jonas <sturmente@proton.me>
2023-02-16 15:07:27 +0100

    Added help command

--- src/io/github/sturmente/stegolib/Main.java
+++ src/io/github/sturmente/stegolib/Main.java
@@ -7,16 +7,20 @@ public class Main {
 	public static void main(String[] args) {
 		
 		if(args.length < 1) {
-			System.out.println("Not enough arguments!");
+			System.out.println("Not enough arguments! Use the help command for further information.");
 			return;
 		}
 		
 		switch(args[0]) {
 			case "lsb":
-				LeastSignificantBit lsb = new LeastSignificantBit();
+				LeastSignificantBit lsb = new LeastSignificantBit(args);
+				lsb.run();
+				break;
+			case "help":
+				LeastSignificantBit.help();
 				break;
 			default:
-				System.out.println("Invalid mode!");
+				System.out.println("Invalid command! Use the help command for further information.");
 				return;
 		}
 	
--- src/io/github/sturmente/stegolib/modes/LeastSignificantBit.java
+++ src/io/github/sturmente/stegolib/modes/LeastSignificantBit.java
@@ -2,8 +2,27 @@ package io.github.sturmente.stegolib.modes;
 
 public class LeastSignificantBit {
 	
-	public LeastSignificantBit() {
+	String[] args;
+	
+	public LeastSignificantBit(String[] args) {
+		this.args = args;
+	}
+	
+	public void run() {
 		System.out.println("Least Significant Bit");
+		
+		if(args.length < 4) {
+			System.out.println("Not enough arguments. Use the help command for further information.");
+			return;
+		}
+			
+			
+	}
+	
+	public static void help() {
+		System.out.println("Least Significant Bit:");
+		System.out.println("lsb encode [input image path] [input data path] [output imgage path]");
+		System.out.println("lsb decode [input image path] [output data path]");
 	}
 	
 }
