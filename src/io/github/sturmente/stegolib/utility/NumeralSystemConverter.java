6f341c2cd4b05c6fdf2c578a8aab2dab89d23f29
Jonas <sturmente@proton.me>
2023-02-17 15:57:15 +0100

    Added binToDec function

--- src/io/github/sturmente/stegolib/utility/NumeralSystemConverter.java
+++ src/io/github/sturmente/stegolib/utility/NumeralSystemConverter.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 public class NumeralSystemConverter {
 		
 		// The first integer in the array is the least significant bit
-		public static ArrayList<Integer> decToBin(int dec, Boolean invert, int trailingZeros) {
+		public static ArrayList<Integer> decToBin(int dec, int trailingZeros) {
 			ArrayList<Integer> res = new ArrayList<Integer>();
 			
 			int rest = dec;
@@ -20,11 +20,21 @@ public class NumeralSystemConverter {
 			// Prevent the array from being empty
 			while(res.size() < trailingZeros) res.add(0);
 			
-			if(!invert) {
-				Collections.reverse(res);
-			}
+			Collections.reverse(res);
 			
 			return res;
+		}
+		
+		public static int binToDec(ArrayList<Integer> bin) {
+			int dec = 0;
+			
+			Collections.reverse(bin);
+			
+			for(int i = 0; i < bin.size(); i++) {
+				dec += bin.get(i) * (Math.pow(2, i));
+			}
+			
+			return dec;
 		}
 	
 }
